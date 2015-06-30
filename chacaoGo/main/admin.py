a92bb52c9eef26d47f1f37e9253803462fb6f5a8
Javier Lopez <javierlopm@hotmail.com>
2015-06-30 12:50:28 -0430

    Inicio de base para interfaces

--- chacaoGo/main/admin.py
+++ chacaoGo/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import User,Event,Comment
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Comment)
--- chacaoGo/main/views.py
+++ chacaoGo/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http      import HttpResponse
+from django.template  import Context,loader
 
 # Create your views here.
+def index(request):
+    t = loader.get_template('base.html')
+    c = Context({'foo': 'bar'})         
+    return HttpResponse(t.render(c))
